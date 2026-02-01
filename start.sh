3801d84cb9f32733b7731b3bab78f5f38f585198
Geniusk-studio <geniusk.studio@gmail.com>
2026-02-01 16:40:38 +0900

    Add files via upload

--- start.sh
+++ start.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Render.com 실행 스크립트
 
-# 웹서버와 cron을 동시에 실행
-php -S 0.0.0.0:${PORT:-8080} server.php &
-php cron.php
+# 웹서버를 메인 프로세스로 실행
+# cron은 백그라운드에서 실행
+php cron.php &
+php -S 0.0.0.0:${PORT:-8080} server.php
