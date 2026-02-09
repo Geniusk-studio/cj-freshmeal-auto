a37fb73e0e4d5c7194b607c34938b8d3c2f41a9b
Geniusk-studio <geniusk.studio@gmail.com>
2026-02-09 23:33:09 +0900

    Update server.php
    test send

--- server.php
+++ server.php
@@ -5,6 +5,9 @@ $uri = $_SERVER['REQUEST_URI'];
 if (strpos($uri, '/trigger.php') !== false || strpos($uri, '/trigger') !== false) {
     // trigger 요청
     require_once 'trigger.php';
+} elseif (strpos($uri, '/test_send.php') !== false || strpos($uri, '/test_send') !== false) {
+    // 테스트 발송 요청
+    require_once 'test_send.php';
 } else {
     // 기본 대시보드
     require_once 'dashboard.php';
